528f65d965c347ce91ec77cf7d6490d5a4c96e59
Prakhar <prakharjoshi960@gmail.com>
2026-07-22 11:34:06 +0530

    security

--- demo/src/main/java/com/dailycodework/demo/Security/user/ShopUserDetails.java
+++ demo/src/main/java/com/dailycodework/demo/Security/user/ShopUserDetails.java
@@ -1,25 +1,86 @@
 package com.dailycodework.demo.Security.user;
 
+import com.dailycodework.demo.model.User;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@NoArgsConstructor
 public class ShopUserDetails implements UserDetails {
+
+    public ShopUserDetails(long id, String email, String password, Collection<GrantedAuthority> authorities) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    private long id;
+    private String email;
+    private String password;
+
+    private Collection<GrantedAuthority> authorities;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAuthorities(Collection<GrantedAuthority> authorities) {
+        this.authorities = authorities;
+    }
+
+
+
+    public static ShopUserDetails buildUserDetails(User user){
+        List<GrantedAuthority> authorities = user.getRoles()
+                .stream()
+                .map(role->new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+
+        return new ShopUserDetails(
+
+                user.getId(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities
+
+        );
+    }
+
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        return "";
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return email;
     }
 
     @Override
--- demo/src/main/java/com/dailycodework/demo/Security/user/ShopUserDetailsService.java
+++ demo/src/main/java/com/dailycodework/demo/Security/user/ShopUserDetailsService.java
@@ -1,14 +1,27 @@
 package com.dailycodework.demo.Security.user;
 
 
-import org.apache.catalina.User;
+
+import com.dailycodework.demo.Repositories.UserRepository;
+import com.dailycodework.demo.model.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
 public class ShopUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public ShopUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = Optional.ofNullable(userRepository.findByEmail(email)).orElseThrow(()-> new UsernameNotFoundException("User not found!"));
+        return ShopUserDetails.buildUserDetails(user);
     }
 }
